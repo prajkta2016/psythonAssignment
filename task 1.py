0739638592b993bfb9be43c75eafcd8cef8b3f35
prajkta2016 <prajktachitranshi09@gmail.com>
2025-05-10 21:52:48 +0530

    added task1.py for assigment 1

---
+++ task 1.py
@@ -0,0 +1,10 @@
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("addtion of 1 st and secod number is :",add)
+print("subtraction of 1 st and secod number is :",sub)
+print("multiplication of 1 st and secod number is :",mul)
+print("division of 1 st and secod number is :",div)
